f0f78e5126ec536ed1c453d4ef05aeb5f251358c
Ashok reddy <akhok.thunga@gmail.com>
2026-06-11 18:44:00 +0530

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,22 @@
+a = 10
+b = 4
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a+b)
+print(a**b)
+print(a-b)
+
+
+
+age1 = input("your age:")
+age2 = input("father age:")
+print("sum of ages:" + age1 + age2)
+
+
+a = 10
+c = '20'
+print(a + int(c))        
+
+
---
+++ welcome.py
@@ -0,0 +1,5 @@
+print("hi,ashok")
+print("baba is a good")
+a = 10
+b = 20
+print(a+b)
